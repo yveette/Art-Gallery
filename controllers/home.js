f77872021dcc6e6f326d5c1a8e12b7143d4f0584
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-22 15:48:09 +0200

    Add catalog page

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,30 @@
+const { isUser } = require('../middleware/guards');
+const preload = require('../middleware/preload');
+const { getAll, sharePubl } = require('../services/publications');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
+router.get('/catalog', async (req, res) => {
+    const publications = await getAll();
+    res.render('catalog', { title: 'Gallery Page', publications });
+});
+
+router.get('/catalog/:id', preload(), (req, res) => {
+    const publication = res.locals.data;
+
+    if (req.session.user) {
+        publication.hasUser = true;
+        publication.isOwner = req.session.user?._id == publication.owner._id;
+
+        if (publication.shared.some(u => u._id == req.session.user._id)) {
+            publication.isJoined = true;
+        }
+    }
+
+    res.render('details', { title: 'Details Page', data: publication });
+});
 });
 
 module.exports = router;
--- services/publications.js
+++ services/publications.js
@@ -4,9 +4,24 @@ const User = require('../models/User');
 async function createPublication(publ) {
     const result = new Publication(publ);
     await result.save();
+
+    //add created id to user 
+    const user = await User.findById(result.owner);
+    user.publications.push(result._id);
+    await user.save();
+}
+
+async function getAll() {
+    return Publication.find({}).lean();
+}
+
+async function getPublById(id) {
+    return Publication.findById(id).populate('owner', 'username').lean();
 }
 
 
 module.exports = {
     createPublication,
+    getAll,
+    getPublById,
 };
